f344dee4951f07b3719d7fb89698fc1f2b541ffb
OsmiumOP <71386574+OsmiumOP@users.noreply.github.com>
2021-07-05 12:17:57 +0530

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'OSMIUM#0001'
+
+description 'Car Dealer Script with FINANCE'
+
+client_scripts {
+    'client/main.lua',
+    'config.lua',
+    'client/showroom.lua',
+    'client/customshowroom.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua'
+}
+
